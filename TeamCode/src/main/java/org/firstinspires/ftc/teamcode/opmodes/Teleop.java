3d4ac85bfaa0eb8f7e725ff32b4f994fa78519f1
FunkyChicken <seanernstes@gmail.com>
2023-11-14 08:53:11 -0500

    Add slow and turbo modes, changed drive for 9499, auto close back things when at intake pos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -50,12 +50,12 @@ public class Teleop extends CommandOpMode
                 new Cmd_SubSys_Drive_JoystickDefault(
                         subSysDrive,
                         telemetry,
-                        () -> -subSysDriverStation.getDriverLeftX(),
+                        subSysDriverStation::getDriverLeftX,
                         subSysDriverStation::getDriverLeftY,
                         subSysDriverStation::getDriverRightX,
                         () -> false,
-                        () -> subSysDriverStation.getCoDriverLeftTrigger() > 0.3,
-                        () -> subSysDriverStation.getCoDriverRightTrigger() > 0.3
+                        () -> (subSysDriverStation.getDriverLeftTrigger() > 0.3),
+                        () -> (subSysDriverStation.getDriverRightTrigger() > 0.3)
                 ));
         subSysArm.setDefaultCommand(
                 new Cmd_SubSys_Arm_JoystickDefault(
